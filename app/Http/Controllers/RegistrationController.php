7a8b956697dda5f3a60cc5a5cc460bcfe2192dc6
jesusavenuechurch <jesusavenuechurch@gmail.com>
2026-03-18 20:05:39 +0200

    dd test

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -63,6 +63,8 @@ class RegistrationController extends Controller
             'phone' => $this->normalizePhone($request->phone),
         ]);
 
+        dd($request->phone);
+
         $organization = Organization::where('slug', $orgSlug)->firstOrFail();
 
         $event = Event::where('slug', $eventSlug)
